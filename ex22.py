4242a4debab9e53d83d48e255cc55df486f2eb97
mleers <mleers4@gmail.com>
2017-07-26 13:53:31 -0700

    Add files via upload

---
+++ ex22.py
@@ -0,0 +1,12 @@
+names = {}
+with open('namefile.txt') as f:
+	line = f.readline()
+	while line:
+		line = line[3:-26]
+		if line in names:
+			names[line] += 1
+		else:
+			names[line] = 1
+		line = f.readline()
+	
+print(names)	
